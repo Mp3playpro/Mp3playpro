29e6558781138e1f68389f4bdf4384d56fcd4f96
Mp3playpro <134804895+Mp3playpro@users.noreply.github.com>
2023-05-28 20:50:20 +0500

    Add files via upload

---
+++ i.mp3play.com/user_v1002b4a7.js
@@ -0,0 +1,4 @@
+var $$_$$ = {
+    user : {"is_user_guest":true,"is_user_login":false,"avatar_url":"","display_name":"","isVerifiedEmail":false}
+};
+(function(d){var j=d.getElementById("nav-user");var n=d.getElementById("nav-user-a");var l=d.getElementById("nav-user-img");var b=d.getElementById("nav-user-name");var k=j.getAttribute("data-ref");var m="nextByApkpure('"+k+"')";if($$_$$.user.is_user_guest){n.setAttribute("href","/br/login");n.setAttribute("onclick",m)}else{n.setAttribute("href","javascript:void(0)");if($$_$$.user.avatar_url){l.setAttribute("src",$$_$$.user.avatar_url)}b.textContent=$$_$$.user.display_name;n.setAttribute("href","/br/u/"+$$_$$.user.id)}})(document);
